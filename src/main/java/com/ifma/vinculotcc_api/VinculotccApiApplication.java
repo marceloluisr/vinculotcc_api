9192e95982fcf95c5904be59b2754330135cbbfd
marceloluisr <marceloluis.pessoal@gmail.com>
2020-12-18 09:58:41 -0300

    Primeiro commit

---
+++ src/main/java/com/ifma/vinculotcc_api/VinculotccApiApplication.java
@@ -0,0 +1,13 @@
+package com.ifma.vinculotcc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinculotccApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinculotccApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifma/vinculotcc_api/VinculotccApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifma.vinculotcc_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinculotccApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
